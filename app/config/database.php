c23439c894d88582d966af8c5117c5125ab217f4
Giel Berkers <info@gielberkers.com>
2013-04-30 19:15:32 +0200

    users migration

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database',
+			'database'  => 'the-game',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'pass',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
